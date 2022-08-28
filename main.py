3261f1be6537636bd823fa90e216934422141fab
B.H Yoo <pikabh@naver.com>
2022-08-28 23:36:08 +0900

    init
    forgotten...

---
+++ main.py
@@ -0,0 +1,102 @@
+## Ex 5-1. QPushButton.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+# from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QPushButton('&Button1', self)
+        btn1.setCheckable(True)
+        btn1.toggle()
+
+        btn2 = QPushButton(self)
+        btn2.setText('Button&2')
+
+        btn3 = QPushButton('Button3', self)
+        btn3.setEnabled(False)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn1)
+        vbox.addWidget(btn2)
+        vbox.addWidget(btn3)
+
+        self.setLayout(vbox)
+        self.setWindowTitle('QPushButton')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+'''
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        lbl_red = QLabel('Red')
+        lbl_green = QLabel('Green')
+        lbl_blue = QLabel('Blue')
+
+        lbl_red.setStyleSheet("color: red;"
+                             "border-style: solid;"
+                             "border-width: 2px;"
+                             "border-color: #FA8072;"
+                             "border-radius: 3px")
+        lbl_green.setStyleSheet("color: green;"
+                               "background-color: #7FFFD4")
+        lbl_blue.setStyleSheet("color: blue;"
+                              "background-color: #87CEFA;"
+                              "border-style: dashed;"
+                              "border-width: 3px;"
+                              "border-color: #1E90FF")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_red)
+        vbox.addWidget(lbl_green)
+        vbox.addWidget(lbl_blue)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('Stylesheet')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        grid.addWidget(QLabel('Title:'), 0, 0)
+        grid.addWidget(QLabel('Author:'), 1, 0)
+        grid.addWidget(QLabel('Review:'), 2, 0)
+
+        grid.addWidget(QLineEdit(), 0, 1)
+        grid.addWidget(QLineEdit(), 1, 1)
+        grid.addWidget(QTextEdit(), 2, 1)
+
+        self.setWindowTitle('QGridLayout')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+'''
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
